18ad17d5701c573213ab2c178914b36c49922854
tomjenkins <Tom.Jenkins@apha.gov.uk>
2023-02-08 13:54:31 +0000

    Added hyperlink quick links to pages from Home page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -16,6 +16,15 @@ const navContent = document.querySelectorAll(".content");
 //
 // ------------------------ //
 
+// Function to hide all content
+const hideContent = function(){
+    // Deactivate bold font in all nav-links (remove 'active' class)
+    navLinks.forEach(link => link.classList.remove("active"));
+
+    // Hide all content (add 'hidden' class to all content pages)
+    navContent.forEach(content => content.classList.add("hidden"));
+};
+
 // Navbar Links and Content Navigation using Event Delegation
 // Purpose: hide all content when any nav-link is clicked then show content for the link clicked
 navBar.addEventListener("click", function(e){
@@ -27,11 +36,8 @@ navBar.addEventListener("click", function(e){
     // If nothing clicked then end function
     if(!clicked) return // guard clause
 
-    // Deactivate bold font in all nav-links (remove 'active' class)
-    navLinks.forEach(link => link.classList.remove("active"));
-
-    // Hide all content (add 'hidden' class to all content pages)
-    navContent.forEach(content => content.classList.add("hidden"));
+    // Hide all content function
+    hideContent();
  
     // Activate bold font for nav-link clicked (add 'active' class)
     clicked.classList.add("active"); 
@@ -45,8 +51,52 @@ navBar.addEventListener("click", function(e){
 });
 
 
-// Help and Support Link on Home Page using Event Delegation TODO
+// Hyperlink to Help and Support page
 // Purpose: hide all content when the Help and Support link is clicked then show Help and Support content
+document.getElementById("hyperlink__toHelp").addEventListener("click", () => {
+
+    // Hide all content function
+    hideContent();
+
+    // Activate bold font for Help and Support page (add 'active' class)
+    document.querySelector(".nav-link-5").classList.add("active");
+
+    // Show content for Help and Support page
+    document.querySelector(".content-5").classList.remove("hidden");
+});
+
+
+// Hyperlink to Cattle Movement Map
+// Purpose: hide all content when Link to Map is clicked then show Cattle Movement Map content
+document.getElementById("hyperlink__toMap").addEventListener("click", () => {
+
+    // Hide all content function
+    hideContent();
+
+    // Activate bold font for Help and Support page (add 'active' class)
+    document.querySelector(".nav-link-2").classList.add("active");
+
+    // Show content for Help and Support page
+    document.querySelector(".content-2").classList.remove("hidden");
+
+    // Redraw cattle movement leaflet map to solve sizing issue on startup
+    map.invalidateSize();
+});
+
+
+// Hyperlink to Nextstrain
+// Purpose: hide all content when Link to Nextstrain is clicked then show Nexstraincontent
+document.getElementById("hyperlink__toNextstrain").addEventListener("click", () => {
+
+    // Hide all content function
+    hideContent();
+
+    // Activate bold font for Help and Support page (add 'active' class)
+    document.querySelector(".nav-link-4").classList.add("active");
+
+    // Show content for Help and Support page
+    document.querySelector(".content-4").classList.remove("hidden");
+});
 
 
 
@@ -56,6 +106,7 @@ navBar.addEventListener("click", function(e){
 //
 // ------------------------ //
 
+// TODO
 // Get screen height
 // const screenHeight = window.innerHeight;
 
