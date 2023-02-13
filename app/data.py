d191600fa0bead8679fb6166f6342cc9674f652c
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-02-13 15:44:48 +0000

    moving some code arround

--- app/data.py
+++ app/data.py
@@ -2,7 +2,9 @@ import os
 import glob
 
 import pandas as pd
+import numpy as np
 
+# TODO: dataclass?
 class Data:
     def __init__(self, data_path):
         self.metadata = pd.read_csv(os.path.join(data_path, "table_1.csv"), 
@@ -17,3 +19,30 @@ class Data:
         for matrix_path in glob.glob(os.path.join(data_path, "*_matrix.csv")):
            self.snps[os.path.basename(matrix_path).split("_", maxsplit=1)[0]] = \
             pd.read_csv(matrix_path, index_col="submission")
+
+    # TODO: handle eartag in query string.
+    # TODO: doctstring
+    def submission_metadata(self, submission):
+        sample_metadata = self.metadata.loc[submission]
+        n_locs = int((sample_metadata.size - 9) / 4)
+        move_dict = {}
+        for loc_num in range(n_locs):
+            if sample_metadata[f"Loc{loc_num+1}"] is np.nan:
+                break
+            move_dict[str(loc_num)] = \
+                {"lat": self.latlon.loc[sample_metadata[f"Loc{loc_num+1}"]].lat,
+                "lon": self.latlon.loc[sample_metadata[f"Loc{loc_num+1}"]].lon,
+                "on_date": sample_metadata[f"Loc{loc_num+1}_StartDate"],
+                "off_date": sample_metadata[f"Loc{loc_num+1}_EndDate"],
+                "type": sample_metadata[f"Loc{loc_num+1}_type"]}
+        return {"submission": submission,
+                "clade": sample_metadata["Clade"],
+                "identifier": sample_metadata["Identifier"],
+                "species": sample_metadata["Host"],
+                "slaughter_date": sample_metadata["SlaughterDate"],
+                "cph": int(sample_metadata["CPH"]),
+                "cphh": int(sample_metadata["CPHH"]),
+                "cph_type": sample_metadata["CPH_type"],
+                "county": sample_metadata["County"],
+                "risk_area": sample_metadata["RiskArea"],
+                "move": move_dict}
--- app/viewbovis.py
+++ app/viewbovis.py
@@ -1,7 +1,4 @@
-import json
-
 from flask import Flask, jsonify, render_template, request
-import numpy as np
 from liveserver import LiveServer
 
 from data import Data
@@ -18,37 +15,14 @@ ls = LiveServer(app)
 def home():
     return ls.render_template("index.html")
 
+# TODO: handle eartag in query string.
 @app.route("/sample")
 def sample_data():
     """
         Returns meta and movement data in json format for a single 
-        sample in respone to a client GET request at the route /sample/
-        with the sample_name encoded in the URL query string. 
+        sample in respone to a client GET request at route /sample/,
+        with the sample_name encoded in the URL query string; e.g. 
+        "/sample?sample_name=AF-61-04255-17". 
     """
     sample_name = request.args.get("sample_name")
-    sample_metadata = data.metadata.loc[sample_name]
-    n_locs = int((sample_metadata.size - 9) / 4)
-    move_dict = {}
-    for loc_num in range(n_locs):
-        if sample_metadata[f"Loc{loc_num+1}"] is np.nan:
-            break
-        move_dict[str(loc_num)] = \
-            {"lat": data.latlon.loc[sample_metadata[f"Loc{loc_num+1}"]].lat,
-             "lon": data.latlon.loc[sample_metadata[f"Loc{loc_num+1}"]].lon,
-             "on_date": sample_metadata[f"Loc{loc_num+1}_StartDate"],
-             "off_date": sample_metadata[f"Loc{loc_num+1}_EndDate"],
-             "type": sample_metadata[f"Loc{loc_num+1}_type"]}
-    sample_metadata_dict = {"submission": sample_name,
-                            "clade": sample_metadata["Clade"],
-                            "identifier": sample_metadata["Identifier"],
-                            "species": sample_metadata["Host"],
-                            "slaughter_date": sample_metadata["SlaughterDate"],
-                            "cph": int(sample_metadata["CPH"]),
-                            "cphh": int(sample_metadata["CPHH"]),
-                            "cph_type": sample_metadata["CPH_type"],
-                            "county": sample_metadata["County"],
-                            "risk_area": sample_metadata["RiskArea"],
-                            "move": move_dict}
-    with open("sample_metadta.json", "w") as outfile:
-        json.dump(sample_metadata_dict, outfile)
-    return jsonify(sample_metadata_dict)
+    return jsonify(data.submission_metadata(sample_name))
