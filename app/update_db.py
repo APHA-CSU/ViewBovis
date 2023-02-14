65ee65210211013618164a18a1f892a0b15a6b34
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-02-14 16:20:25 +0000

    changes

--- app/update_db.py
+++ app/update_db.py
@@ -19,7 +19,6 @@ def run():
     for matrix_path in glob.glob(os.path.join(data_path, "*_matrix.csv")):
         df_snps[os.path.basename(matrix_path).split("_", maxsplit=1)[0]] = \
         pd.read_csv(matrix_path, index_col="submission")
-    # connect to sqlite db
     conn = sqlite3.connect("viewbovis.db")
     # write metadata to sqlite db
     df_metadata.to_sql("metadata", con=conn, if_exists="replace")
@@ -27,8 +26,8 @@ def run():
     df_latlon.to_sql("latlon", con=conn, if_exists="replace")
     # write snp data to sqlite db - 1 table per sample
     for sample in df_snps.columns:
-        df_snps.loc[:, sample].to_frame().rename({sample: "snp_dist"}, axis=1).\
-            to_sql(sample, con=conn, if_exists="replace")
+        df_snps.loc[:, sample].to_frame().rename({sample: "snp_distance"}, 
+            axis=1).to_sql(sample, con=conn, if_exists="replace")
 
 if __name__ == "__main__":
     run()
