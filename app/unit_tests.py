6b7a34d52214e93c2c24db0cd5af2127284d3c2b
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-03-17 17:25:01 +0000

    working test_submisison_movement test

--- app/unit_tests.py
+++ app/unit_tests.py
@@ -20,6 +20,10 @@ class TestViewBovisData(unittest.TestCase):
             mock_connect.return_value.close.return_value = mock_close()
             self.data = ViewBovisData("foo_path")
 
+    def tearDown(self) -> None:
+        self.data.__del__()
+        return super().tearDown()
+
     def test_clean_metadata(self):
         # test usual input
         df_test_input = pd.DataFrame({"A": [None], "B": [None], "C": [None],
@@ -36,6 +40,42 @@ class TestViewBovisData(unittest.TestCase):
         df_test_input = pd.DataFrame({"A": [None, None]})
         with self.assertRaises(Exception):
             self.data._clean_metadata(df_test_input)
+
+    def test_submission_movement_metadata(self):
+        self.data._submission_metadata = mock.Mock()
+        self.data._clean_metadata = mock.Mock(wraps=lambda x: x)
+        self.data._submission_metadata.return_value = \
+            pd.DataFrame({"Clade": ["D"],
+                          "Identifier": ["G"],
+                          "Host": ["J"],
+                          "SlaughterDate": ["M"],
+                          "CPH": ["P"],
+                          "CPHH": ["S"],
+                          "CPH_Type": ["V"],
+                          "County": ["Y"],
+                          "RiskArea": ["AB"],
+                          "Loc0": ["AE"],
+                          "Loc0_Type": ["AH"],
+                          "Loc0_StartDate": ["AK"],
+                          "Loc0_Duration": ["AN"],
+                          "Loc0_EndDate": ["AQ"],
+                          "Loc1": ["AT"],
+                          "Loc1_Type": ["AW"],
+                          "Loc1_StartDate": ["AZ"],
+                          "Loc1_Duration": ["BC"],
+                          "Loc1_EndDate": ["BF"],
+                          "Loc2": ["BI"],
+                          "Loc2_Type": ["BL"],
+                          "Loc2_StartDate": ["BO"],
+                          "Loc2_Duration": ["BR"],
+                          "Loc2_EndDate": ["BU"]},
+                         index=["A"])
+        self.data._get_lat_long = mock.Mock()
+        self.data._get_lat_long.return_value = \
+            pd.DataFrame({"Lat": [1, 2, 3],
+                          "Long": [10, 11, 12]},
+                         index=["AE", "AT", "BI"])
+        print(self.data.submission_movement_metadata("A"))
 
 
 if __name__ == "__main__":
