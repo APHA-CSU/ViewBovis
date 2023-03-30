d3cef60e3902329419b6ddfff7885dfba5657646
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-03-30 15:56:17 +0100

    reformat

--- app/viewbovis_data.py
+++ app/viewbovis_data.py
@@ -18,7 +18,7 @@ class InvalidIdException(Exception):
 class ViewBovisData:
     def __init__(self, data_path):
         self._matrix_dir = path.join(data_path, "snp_matrix")
-        db_path = path.join(data_path, "viewbovis.db")
+        db_path = path.join(data_path, "viewbovis_29032023.db")
         self._db = sqlite3.connect(f"file:{db_path}?mode=ro", uri=True)
         self._cursor = self._db.cursor()
 
@@ -41,29 +41,25 @@ class ViewBovisData:
                                  index_col="Submission",
                                  params=ids+ids)
 
-    def _clean_metadata(self, df_metadata: pd.DataFrame) -> pd.DataFrame:
-        """
-            Removes NULL columns from location data in metadata
-            DataFrame. Use only for DataFrame with single row.
-        """
-        if len(df_metadata) > 1:
-            # TODO: custom exception
-            raise Exception("DataFrame must contain only one row")
-        # TODO: below is dropping location columns with NULL but leaving
-        # all other columns this is done by splitting the df and
-        # re-joining after: there is probably a nicer way to do this.
-        df_metadata_0 = df_metadata[df_metadata.columns[:9]]
-        df_metadata_1 = df_metadata[df_metadata.columns[9:]].dropna(axis=1)
-        return df_metadata_0.join(df_metadata_1)
-
     # TODO: validate input
+    def _submission_movdata(self, submission: str) -> pd.DataFrame:
+        """
+            Fetches movement data for a given Submission. Returns an
+            empty DataFrame if no data exists.
+        """
+        query = """SELECT * FROM movements WHERE Submission=:submission"""
+        return pd.read_sql_query(query,
+                                 self._db,
+                                 index_col="Submission",
+                                 params=submission)
+
     def _get_lat_long(self, cphs: list) -> tuple:
         """
             Fetches latitude and longitude for a given a list of CPHs.
             Returns a DataFrame with columns 'lat' and 'lon' and the
             corresponding CPH in the index.
         """
-        query = f"""SELECT * FROM latlon WHERE CPH IN
+        query = f"""SELECT Lat,Long FROM latlon WHERE CPH IN
                    ({','.join('?' * len(cphs))})"""
         return pd.read_sql_query(query,
                                  self._db,
@@ -104,49 +100,36 @@ class ViewBovisData:
         """
             Returns metadata and movement data for 'id' as a dictionary.
         """
-        # get cleaned metadata for a single id
-        df_metadata_sub = self._submission_metadata([id])\
-            .pipe(self._clean_metadata)
+        # get metadata for a single id
+        df_metadata_sub = self._submission_metadata([id])
         if df_metadata_sub.empty:
             raise InvalidIdException(
                     f"'{id}' does not match a valid eartag or AF-number")
-        # get valid location numbers (missing data is not valid)
-        loc_nums_valid = [self._extract_location_number(i)
-                          for i in df_metadata_sub.columns[9::5]]
-        df_cph_latlon_map = \
-            self._get_lat_long(
-                [df_metadata_sub[f"Loc{loc_num}"][0]
-                    for loc_num in loc_nums_valid])
-        # get total number of locations (inc. missing data)
-        n_locs = self._extract_location_number(df_metadata_sub.columns[-5]) + 1
+        df_movements = self._submission_movdata(df_metadata_sub.index[0])
+        df_cph_latlon_map = self._get_lat_long(df_movements["Loc"].to_list())
         # construct dictionary of movement data
-        move_dict = {(str(loc_num)):
-                     ({"cph":
-                           df_metadata_sub[f"Loc{loc_num}"][0],
-                       "lat":
-                           df_cph_latlon_map["Lat"][df_metadata_sub[f"Loc{loc_num}"][0]],
-                       "lon":
-                           df_cph_latlon_map["Long"][df_metadata_sub[f"Loc{loc_num}"][0]],
-                       "on_date":
-                           df_metadata_sub[f"Loc{loc_num}_StartDate"][0],
-                       "off_date":
-                           df_metadata_sub[f"Loc{loc_num}_EndDate"][0],
-                       "stay_length":
-                           df_metadata_sub[f"Loc{loc_num}_Duration"][0],
-                       "type":
-                           df_metadata_sub[f"Loc{loc_num}_Type"][0]}
-                     if loc_num in loc_nums_valid else "missing data point")
-                     for loc_num in range(n_locs)}
+        move_dict = {str(row["Loc_Num"]):
+                     {"cph": row["Loc"][0],
+                      "lat": df_cph_latlon_map["Lat"][row["Loc"]],
+                      "lon": df_cph_latlon_map["Long"][row["Loc"]],
+                      "on_date": row["Loc_StartDate"],
+                      "off_date": row["Loc_EndDate"],
+                      "stay_length": row["Loc_Duration"],
+                      "type": row["CPH_Type"],
+                      "county": row["County"]}
+                     for _, row in df_movements.iterrows()}
         return {"submission": df_metadata_sub.index[0],
                 "clade": df_metadata_sub["Clade"][0],
                 "identifier": df_metadata_sub["Identifier"][0],
                 "species": df_metadata_sub["Host"][0],
+                "animal_type": df_metadata_sub["Animal_Type"][0],
                 "slaughter_date": df_metadata_sub["SlaughterDate"][0],
                 "cph": df_metadata_sub["CPH"][0],
                 "cphh": df_metadata_sub["CPHH"][0],
                 "cph_type": df_metadata_sub["CPH_Type"][0],
                 "county": df_metadata_sub["County"][0],
                 "risk_area": df_metadata_sub["RiskArea"][0],
+                "out_of_homerange": df_metadata_sub["OutsideHomeRange"][0],
                 "move": move_dict}
 
     # TODO: not just cows
@@ -207,6 +190,8 @@ class ViewBovisData:
                  "snp_distance":
                     int(df_snps_related_processed["snp_dist"][index]),
                  "animal_id": row["Identifier"],
+                 "herd": row["CPHH"],
+                 "clade": row["Clade"],
                  "date": row["SlaughterDate"]}
                 for index, row in df_metadata_related.iterrows()
                 if row["Host"] == "COW"}
