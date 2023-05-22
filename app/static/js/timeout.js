39a92865508795cb875d0dde8ecb482e1b065bd7
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-05-22 14:56:14 +0100

    timeout on clientside

--- app/static/js/timeout.js
+++ app/static/js/timeout.js
@@ -6,13 +6,36 @@ var inactivityTime = function () {
     document.onkeydown = resetTimer;
 
     function logout() {
-        fetch('/timeout').then((response) => {
-            return response.text();
-        })
-        .then((html) => {
-            document.body.innerHTML = html     
-        });
-    }
+        const url = window.location.protocol + "//" + window.location.hostname;
+        document.write(
+            `<html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>ViewBovis</title>
+                    <!-- CSS -->
+                    <link rel="stylesheet" href=` + url + `/static/css/main.css">
+                </head>
+                <body>
+                    <div class="container-fluid content content-1">
+                        <!-- ViewBovis Logo and Description -->
+                        <div class="row align-items-center">
+                            <!-- Column: logo -->
+                            <div class="col-1">
+                                <img src="/static/img/VBIcon16_APHAGreen.svg" class="home-logo">
+                            </div>
+                            <!-- Column: description -->
+                            <div class="col-8">
+                                <div class="home-description-container">
+                                    <p class="home-description fs-5">
+                                        <span class="fw-bold">Session timeout:</span> your ViewBovis session has expired, please <a class="text-hyperlink" href="https://hosting.int.sce.network/global-protect/login.esp" target="_blank">login again</a>.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                </body>
+            </html>`)
+        }
 
     function resetTimer() {
         clearTimeout(time);
