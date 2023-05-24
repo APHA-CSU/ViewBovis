7192120cbec8cad1aa64be089083a834c960a561
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-05-24 16:22:13 +0100

    save to json by day

--- parse_nginx_log.py
+++ parse_nginx_log.py
@@ -1,21 +1,23 @@
 import argparse
+import json
+from datetime import date
 
 DEFAULT_ACCESS_LOG_PATH = "/var/log/nginx/access.log"
 
 
 def run(access_log_path):
-    # TODO: add nextstrain requests
-    requests = {"GET / ": 0,
-                "GET /sample": 0,
-                "GET /sample/related": 0,
-                "GET /sample/matrix": 0,
-                "GET /charon/getDataset": 0}
-    with open(access_log_path, "r") as f:
-        for line in f:
+    requests = {"/ ": 0,
+                "/sample": 0,
+                "/sample/related": 0,
+                "/sample/matrix": 0,
+                "/charon/getDataset": 0}
+    with open(access_log_path, "r") as f_log:
+        for line in f_log:
             for route in requests.keys():
                 if f"GET {route}" in line:
                     requests[route] += 1
-    print(requests)
+    with open(f"viewbovis_requests_{date.today()}.json", "w") as f_json:
+        json.dump(requests, f_json, indent=4)
 
 
 if __name__ == "__main__":
