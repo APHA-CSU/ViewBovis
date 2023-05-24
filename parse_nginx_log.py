e9fe3d790f33a063f558023b5aa1a514e84adc48
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-05-24 16:49:39 +0100

    proper output path

--- parse_nginx_log.py
+++ parse_nginx_log.py
@@ -1,11 +1,13 @@
 import argparse
 import json
+import os
 from datetime import date
 
 DEFAULT_ACCESS_LOG_PATH = "/var/log/nginx/access.log"
+DEFAULT_OUTPUT_PATH = "/var/log/viewbovis/"
 
 
-def run(access_log_path):
+def run(access_log_path, out_dir):
     requests = {"/ ": 0,
                 "/sample": 0,
                 "/sample/related": 0,
@@ -16,7 +18,8 @@ def run(access_log_path):
             for route in requests.keys():
                 if f"GET {route}" in line:
                     requests[route] += 1
-    with open(f"viewbovis_requests_{date.today()}.json", "w") as f_json:
+    with open(os.path.join(out_dir, f"viewbovis_requests_{date.today()}.json"),
+              "w") as f_json:
         json.dump(requests, f_json, indent=4)
 
 
@@ -25,5 +28,8 @@ if __name__ == "__main__":
     parser.add_argument("--log_path", dest="access_log_path",
                         default=DEFAULT_ACCESS_LOG_PATH,
                         help="path to nginx access log file")
+    parser.add_argument("--out_dir", dest="out_dir",
+                        default=DEFAULT_ACCESS_LOG_PATH,
+                        help="path to output directory")
     args = parser.parse_args()
-    run(args.access_log_path)
+    run(args.access_log_path, args.out_dir)
