ccdc586f6050f080a1662c42ba14c8dcad6f9690
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-07-26 14:22:14 +0100

    fix nextsrain url

--- app/static/js/nextstrain.js
+++ app/static/js/nextstrain.js
@@ -206,7 +206,7 @@ const renderNextstrain = function(URL){
     // TODO: uncomment below for production mode
     //document.getElementById("nextstrain-container-id").insertAdjacentHTML("afterbegin", `
     //    <div id="nextstrain-div">
-    //        <iframe src="http://127.0.0.1:4001/${URL}" id="nextstrain-iframe" frameborder="0" height="${iframeHeight}px" width="100%"></iframe>
+    //        <iframe src="https://hosting.int.sce.network/http/nextstrain-beta.int.sce.network/${URL}" id="nextstrain-iframe" frameborder="0" height="${iframeHeight}px" width="100%"></iframe>
     //    </div>
     //`);
 
