e09fdbfcaaaed9c4376d1faa42aa4c7b0aa07069
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-07-27 17:21:35 +0100

    remove unecessary expose command

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 DATA_DIR=$1
-docker run --rm -d --expose 5000 --network host --name viewbovis \
+docker run --rm -d --network host --name viewbovis \
     --mount type=bind,source=$DATA_DIR,target=/data \
     --mount type=bind,source=/var/log/viewbovis-access.log,target=/ViewBovis/access.log \
     --mount type=bind,source=/var/log/viewbovis-error.log,target=/ViewBovis/error.log \
