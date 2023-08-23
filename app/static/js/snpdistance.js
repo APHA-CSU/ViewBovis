1d16d3bee5c67d345f8d9d84b30b1245b090b29e
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-08-23 11:42:02 +0100

    use all samples, inc. SOI in autozoom

--- app/static/js/snpdistance.js
+++ app/static/js/snpdistance.js
@@ -852,9 +852,12 @@ const renderRelatedMarkers = function (json, target) {
     relatedMarker.bindPopup(popupContentSNPMap(item, item.submission), cowheadPopupOptions2);
   });
 
+  let all_samples = Object.values({...json})
+  console.log(relatedSampleArr)
+  console.log(all_samples)
   // Create a new array in the format [ [lat1, lon1], [lat2, lon2], [..., ...] ]
-  const allLat = relatedSampleArr.map( arr => arr.lat ); 
-  const allLon = relatedSampleArr.map( arr => arr.lon ); 
+  const allLat = all_samples.map( arr => arr.lat ); 
+  const allLon = all_samples.map( arr => arr.lon ); 
   const allPts = allLat.map( (lat, index) => { return [lat, allLon[index]] });
 
   // Automatically zoom in on the markers and allow some padding (buffer) to ensure all points are in view
@@ -896,6 +899,7 @@ const showRelatedSamples = async function () {
     if(!response.ok) throw new Error("Problem getting SNP data from backend");
     let json = await response.json();
     
+    console.log(json)
     // Remove spinner when fetch is complete
     document.getElementById("snpmap-spinner").classList.add("hidden");
 
