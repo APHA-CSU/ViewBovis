25fc4ef3d2bdadea7715e4d5964191fc8b4ecaed
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-08-24 17:48:04 +0100

    stable

--- app/e2e_tests.py
+++ app/e2e_tests.py
@@ -5,6 +5,7 @@
 """
 
 import unittest
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -12,6 +13,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common import exceptions
 
 CHROMEDRIVER_PATH = "/usr/local/bin/chromedriver"
 
@@ -29,14 +31,15 @@ class E2ETests(unittest.TestCase):
         self.driver = webdriver.Chrome(service=s, options=chrome_options)
         # connect to ViewBovis
         self.driver.get("http://127.0.0.1:5000")
+        # pass security modal
+        security_modal = self.driver.find_element(By.ID, "checkbox--agree")
+        security_modal.click()
+        self.wait = WebDriverWait(self.driver, 10)
 
     def tearDown(self) -> None:
         self.driver.close()
 
-    def test_snp_map(self):
-        # pass security modal
-        security_modal = self.driver.find_element(By.ID, "checkbox--agree")
-        security_modal.click()
+    def test_snp_map_setup(self):
         # navigate to snp map
         snp_dist_btn = self.driver.find_element(By.ID, "snp_distance_tab")
         snp_dist_btn.click()
@@ -47,32 +50,40 @@ class E2ETests(unittest.TestCase):
         search_box.send_keys("a_id")
         snp_distance = self.driver.find_element(By.ID, "snp-distance-value")
         self.driver.execute_script("arguments[0].textContent = arguments[1];",
-                                   snp_distance, "1")
+                                   snp_distance, "5")
         plot_isolates_btn = \
             self.driver.find_element(By.ID, "btn__plot-related-isolates")
         plot_isolates_btn.click()
         # wait until map icons have loaded and find related isolate icon
-        wait = WebDriverWait(self.driver, 10)
-        related_icon = \
-            wait.until(EC.visibility_of_any_elements_located((By.XPATH,
-                       "//div[@class='awesome-number-marker-icon-gray awesome-number-marker marker-b_submission leaflet-zoom-animated leaflet-interactive']")))
-        # assert the map location
-        # NOTE: this doesn't actually match what's in the browser but
-        # matches what's returned by selenium
-        self.assertEqual(related_icon[0].get_attribute("style"),
-                         "margin-left: -17px; margin-top: -42px; width: 35px; height: 45px; transform: translate3d(698px, 730px, 0px); z-index: 730;")
+        related_icon_number = \
+            self.wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                              "//*[@id='map2']/div[1]/div[4]/div/i")))
         # assert icon number is white, i.e. not selected
-        related_icon_number = self.driver.find_element(By.XPATH, "//*[@id='map2']/div[1]/div[4]/div/i")
         self.assertEqual(related_icon_number.get_attribute("style"), "color: white;")
         # show the table
         show_table_btn = self.driver.find_element(By.ID, "show-table")
         show_table_btn.click()
         # click the row for the related isolate
-        row = self.driver.find_element(By.XPATH, "//*[@id='table-content-container']/div[2]/div/div[2]")
+        row = \
+            self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                        "//*[@id='table-content-container']/div[2]/div/div[2]")))
+        # hacky way to ensure that the row is in clickable state - will
+        # try to click 10 times over the space of 1 second, if still not
+        # clickable on the 10th try an Exception is raised.
+        for _ in range(9):
+            try:
+                print(_)
+                row.click()
+                break
+            except exceptions.ElementClickInterceptedException:
+                time.sleep(0.1)
         row.click()
         # assert that the related isolate number has changed colour
-        related_icon_number = self.driver.find_element(By.XPATH, "//*[@id='map2']/div[1]/div[4]/div/i")
-        self.assertEqual(related_icon_number.get_attribute("style"), "color: rgb(255, 190, 51);")
+        related_icon_number = \
+            self.driver.find_element(By.XPATH,
+                                     "//*[@id='map2']/div[1]/div[4]/div/i")
+        self.assertEqual(related_icon_number.get_attribute("style"),
+                         "color: rgb(255, 190, 51);")
 
 
 if __name__ == "__main__":
