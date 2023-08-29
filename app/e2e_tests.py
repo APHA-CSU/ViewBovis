7347449781ead905a8cf308df960b4a80a2290c1
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-08-29 10:57:57 +0100

    assert row clicking works

--- app/e2e_tests.py
+++ app/e2e_tests.py
@@ -54,14 +54,6 @@ class E2ETests(unittest.TestCase):
         plot_isolates_btn = \
             self.driver.find_element(By.ID, "btn__plot-related-isolates")
         plot_isolates_btn.click()
-        # wait until map icons have loaded and find related isolate icon
-        parent_div = \
-            self.wait.until(
-                EC.visibility_of_element_located((By.XPATH,
-                                                  "//div[@class='awesome-number-marker-icon-gray awesome-number-marker marker-b_submission leaflet-zoom-animated leaflet-interactive']")))
-        related_icon_number = parent_div.find_element(By.TAG_NAME, "i")
-        # assert icon number is white, i.e. not selected
-        self.assertEqual(related_icon_number.get_attribute("style"), "color: white;")
         # show the table
         show_table_btn = self.driver.find_element(By.ID, "show-table")
         show_table_btn.click()
@@ -72,30 +64,36 @@ class E2ETests(unittest.TestCase):
                                                   "//div[@class='tabulator-table']")))
         rows = table.find_elements(By.XPATH, ".//div[@role='row']")
         rows_dict = {}
+        # loop through rows
         for row in rows:
-            id = row.find_element(By.XPATH, ".//div[@tabulator-field='animal_id']")
-            rows_dict[id.text] = row
-
-        print(rows_dict)
-        ## click the row for the related isolate
-        #row = \
-            #self.wait.until(EC.element_to_be_clickable((By.XPATH,
-                                                        #"//*[@id='table-content-container']/div[2]/div/div[2]")))
-        ## hacky way to ensure that the row is in clickable state - will
-        ## try to click 10 times over the space of 1 second, if still not
-        ## clickable on the 10th try an Exception is raised.
-        #for _ in range(9):
-            #try:
-                #print(_)
-                #row.click()
-                #break
-            #except exceptions.ElementClickInterceptedException:
-                #time.sleep(0.1)
-        #row.click()
-        ## assert that the related isolate number has changed colour
-        #related_icon_number = parent_div.find_element(By.TAG_NAME, "i")
-        #self.assertEqual(related_icon_number.get_attribute("style"),
-                         #"color: rgb(255, 190, 51);")
+            # get the submission element for the row
+            sub_element = row.find_element(By.XPATH,
+                                           ".//div[@tabulator-field='submission']")
+            rows_dict[sub_element.text] = row
+        # loop through clickable rows (with associated map icons)
+        for sub in ["b", "c", "d"]:
+            # locate the associated submission on the map
+            map_sub_div_element = \
+                self.driver.find_element(By.XPATH,
+                                         f"//div[@class='awesome-number-marker-icon-gray awesome-number-marker marker-{sub}_submission leaflet-zoom-animated leaflet-interactive']")
+            related_icon_number = map_sub_div_element.find_element(By.TAG_NAME, "i")
+            # assert icon number is white, i.e. not selected
+            self.assertEqual(related_icon_number.get_attribute("style"), "color: white;")
+            # hacky way to ensure that the row is in clickable state - will
+            # try to click 10 times over the space of 1 second, if still not
+            # clickable on the 10th try an Exception is raised.
+            for _ in range(9):
+                try:
+                    rows_dict[f"{sub}_submission"].click()
+                    break
+                except exceptions.ElementClickInterceptedException:
+                    time.sleep(0.1)
+                rows_dict[f"{sub}_submission"].click()
+            # assert that the related isolate number has changed colour
+            related_icon_number = \
+                map_sub_div_element.find_element(By.TAG_NAME, "i")
+            self.assertEqual(related_icon_number.get_attribute("style"),
+                             "color: rgb(255, 190, 51);")
 
 
 if __name__ == "__main__":
