128bd11ad9f17546bde882bb7746ac10404ab195
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-09-04 17:34:36 +0100

    handle missing cph in snp-map

--- app/viewbovis_data.py
+++ app/viewbovis_data.py
@@ -47,7 +47,10 @@ class Request:
             # retrieve x,y and lat,lon into tuples
             df_cph_2_osmapref = \
                 self._get_os_map_ref(self._df_metadata_soi["CPH"])
-            self._xy = tuple(df_cph_2_osmapref.iloc[0, 2:].values.flatten())
+            if not self._df_metadata_soi["CPH"][0]:
+                self._xy = None
+            else:
+                self._xy = tuple(df_cph_2_osmapref.iloc[0, 2:].values.flatten())
         # if missing metadata
         else:
             # get WGS metadata for the SOI
@@ -332,6 +335,8 @@ class Request:
         # TODO: workout how to include SOI without metadata
         if self._df_metadata_soi.empty:
             raise NoMetaDataException(self._id)
+        elif self._xy is None:
+            raise NoMetaDataException(self._id)
         df_snps_related = self._related_snp_matrix(snp_threshold)
         # get metadata for all related submissions
         df_metadata_related = \
@@ -410,7 +415,8 @@ class Request:
         snps_related = df_snps_related.copy().stack().\
             reset_index().values.tolist()
         return {"soi": self._submission,
-                "identifier": self._df_metadata_soi["Identifier"][0],
+                "identifier": None if self._df_metadata_soi.empty
+                else self._df_metadata_soi["Identifier"][0],
                 "sampleIDs": submissions,
                 "matrix": snps_related}
 
