498f790e8a8831daacac98d3df3e39aed2971cf0
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-09-19 17:14:34 +0100

    expected first in asserts

--- app/tests/e2e_tests.py
+++ app/tests/e2e_tests.py
@@ -103,7 +103,7 @@ class E2ETests(unittest.TestCase):
                                          f"//div[@class='awesome-number-marker-icon-gray awesome-number-marker marker-{sub}_submission leaflet-zoom-animated leaflet-interactive']")
             related_icon_number = map_sub_div_element.find_element(By.TAG_NAME, "i")
             # assert icon number is white, i.e. not selected
-            self.assertEqual(related_icon_number.get_attribute("style"), "color: white;")
+            self.assertEqual("color: white;", related_icon_number.get_attribute("style"))
             # hacky way to ensure that the row is clickable: will try to
             # click 10 times over 1 second, if still not clickable on
             # the 10th try an Exception is raised.
@@ -126,7 +126,7 @@ class E2ETests(unittest.TestCase):
             pop_up_header = \
                 self.wait.until(EC.visibility_of_element_located((By.XPATH,
                                                                   "//*[@id='map2']/div[1]/div[6]/div/div[1]/div/div[1]")))
-            self.assertEqual(pop_up_header.text, f"{sub}_id")
+            self.assertEqual(f"{sub}_id", pop_up_header.text)
             # click the map icon - make pop-up go away
             map_sub_div_element.click()
             self.wait.until(EC.invisibility_of_element(pop_up_header))
