6e7dcf03a9dc61a406b7d847ba64ab06099c2db7
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-11-02 15:47:18 +0000

    add nextstrain data

--- server/s3_data_download.sh
+++ server/s3_data_download.sh
@@ -1,3 +1,4 @@
 S3_URI=$1
 sudo aws s3 cp ${S3_URI}/Metadata/ /ViewBovis/. --recursive
 sudo aws s3 cp ${S3_URI}/snp-matrix/ /ViewBovis/snp_matrix/ --recursive
+aws s3 cp ${S3_URI}/jsonExport/ /home/ranch-159/NextstrainData/jsonExport/ --recursive
