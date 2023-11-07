559248803318853f6bdd3a87d434b0f3a5a3e5a4
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-11-07 14:54:16 +0000

    remove recursive

--- server/s3_data_download.sh
+++ server/s3_data_download.sh
@@ -1,4 +1,4 @@
 S3_URI=$1
-sudo aws s3 cp ${S3_URI}/Metadata/viewbovis.db /ViewBovis/. --recursive
+sudo aws s3 cp ${S3_URI}/Metadata/viewbovis.db /ViewBovis/.
 sudo aws s3 cp ${S3_URI}/snp-matrix/ /ViewBovis/snp_matrix/ --recursive
 aws s3 cp ${S3_URI}/jsonExport/ /home/ranch-159/NextstrainData/jsonExport/ --recursive
