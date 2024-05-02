4e0a1868aae3c7c4f899d1e84cfbea502be80608
Sainab Omar <sainabomar07@gmail.com>
2024-05-02 19:32:11 +0100

    creates nextstrain page

--- app/front_end/src/App.js
+++ app/front_end/src/App.js
@@ -1,20 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 import "./App.css";
 import Home from "./components/Home/Home";
 import CattleMovement from "./components/CattleMovement/CattleMovement";
-import SNPdistance from "./components/SNPdistance/SNPdistance";
+import SNPdistance from "./components/SNPDistance/SNPDistance";
+import SNPMap from "./components/SNPDistance/SNPMap";
+import SNPMatrix from "./components/SNPDistance/SNPMatrix";
+import Nextstrain from "./components/Nextstrain/Nextstrain";
 import NavbarComp from "./components/Navbar/NavbarComp";
 
 function App() {
   return (
     <Container fluid className="app">
-        <NavbarComp/>
+      <NavbarComp />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cattlemovement" element={<CattleMovement />} />
         <Route path="/snpdistance" element={<SNPdistance />} />
+        <Route path="/snpdistance/snpmap" element={<SNPMap />} />
+        <Route path="/snpdistance/snpmatrix" element={<SNPMatrix />} />
+        <Route path="/nextstrain" element={<Nextstrain />} />
       </Routes>
     </Container>
   );
