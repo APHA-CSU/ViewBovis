eb4dc7a32aac3b6c9a02a264213ff9bd31971983
Sainab Omar <sainabomar07@gmail.com>
2024-05-02 10:40:37 +0100

    renames directory

---
+++ app/front_end/src/components/MapSidebar/MapSidebar.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Accordion from "react-bootstrap/Accordion";
+import Form from "react-bootstrap/Form";
+import Button from "@govuk-react/button";
+import Heading from "@govuk-react/heading";
+import Input from "@govuk-react/input";
+
+const MapSidebar = ({ setSearchSample }) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchSample(searchInput);
+    setSearchInput("");
+  };
+  return (
+    <div>
+      <form className="sample-search" onSubmit={handleSubmit}>
+        <div>
+          <Heading id="enter-sample-container" size="MEDIUM">
+            Enter Identifier or Submission
+          </Heading>
+          <Input value={searchInput} onChange={handleChange} />
+          <Button className="my-2" buttonColour="#00a33b">
+            Show Cattle Movement
+          </Button>
+        </div>
+      </form>
+      <Row className="my-4">
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Add Another Movement</Accordion.Header>
+            <Accordion.Body>TBC</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Row> 
+      <Row className="my-5">
+        <Heading size="SMALL">LAYERS</Heading>
+        <Form>
+          <Form.Check className="checkbox" label="Risk Areas" />
+          <Form.Check className="checkbox" label="Counties" />
+          <Form.Check className="checkbox" label="TB Hotspots" />
+          <Form.Check className="checkbox" label="Movement Lines" />
+        </Form>
+      </Row>
+      <hr></hr>
+    </div>
+  );
+};
+export default MapSidebar;
