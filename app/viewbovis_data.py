224ef3e9229596f3f383beef0302962606b96d03
ellisrichardj <richard.ellis@apha.gov.uk>
2024-05-16 23:40:53 +0100

    revert Remove none-type returns as breaks test

--- app/viewbovis_data.py
+++ app/viewbovis_data.py
@@ -110,7 +110,10 @@ class Request:
         exclusion = pd.read_sql_query(query, self._db,
                                       params={"submission":
                                               self._submission})
-        return exclusion["Exclusion"][0]
+        if exclusion.empty:
+            return None
+        else:
+            return exclusion["Exclusion"][0]
 
     def _sample_to_submission(self, sample: str) -> str:
         """
@@ -120,6 +123,8 @@ class Request:
         query = "SELECT * FROM wgs_metadata WHERE Sample=:sample"
         df_wgs_sub = pd.read_sql_query(query, self._db,
                                        params={"sample": sample})
+        if df_wgs_sub.empty:
+            return None
         return df_wgs_sub["Submission"][0]
 
     def _query_movdata(self, submission: str) -> pd.DataFrame:
@@ -371,7 +376,9 @@ class Request:
                 NoMetaDataException: for missing CPH in metadata for the
                     SOI
         """
-        if self._df_metadata_soi.empty or self._xy is None:
+        if self._df_metadata_soi.empty:
+            raise NoMetaDataException(self._id)
+        elif self._xy is None:
             raise NoMetaDataException(self._id)
         df_snps_related = self._related_snp_matrix(snp_threshold)
         # get metadata for all related submissions
