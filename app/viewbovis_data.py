9b752d61702c5297013d65e8d691f5799f070af1
praveenmurugan101 <praveenmurugan@ranch-653.int.sce.network>
2024-05-20 11:54:07 +0100

    no WGS data submission fixes

--- app/viewbovis_data.py
+++ app/viewbovis_data.py
@@ -110,7 +110,10 @@ class Request:
         exclusion = pd.read_sql_query(query, self._db,
                                       params={"submission":
                                               self._submission})
-        return exclusion["Exclusion"][0]
+        if exclusion.empty:
+            return None
+        else:
+            return exclusion["Exclusion"][0]
 
     def _sample_to_submission(self, sample: str) -> str:
         """
@@ -120,6 +123,8 @@ class Request:
         query = "SELECT * FROM wgs_metadata WHERE Sample=:sample"
         df_wgs_sub = pd.read_sql_query(query, self._db,
                                        params={"sample": sample})
+        if df_wgs_sub.empty:
+            return None
         return df_wgs_sub["Submission"][0]
 
     def _query_movdata(self, submission: str) -> pd.DataFrame:
