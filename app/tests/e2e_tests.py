34ca354aad6c4e3c209204b8a584a09b1a6eff61
ellisrichardj <richard.ellis@apha.gov.uk>
2024-05-20 23:37:57 +0100

    test uppercase

--- app/tests/e2e_tests.py
+++ app/tests/e2e_tests.py
@@ -84,7 +84,7 @@ class E2ETests(unittest.TestCase):
             self.assertNotIn(f"{sub}_submission", rows_dict.keys())
         # assert the SOI row is not clickable
         self.assertIn("tabulator-unselectable",
-                      rows_dict[f"{soi}_submission"].get_attribute("class"),
+                      rows_dict[f"{soi}_SUBMISSON"].get_attribute("class"),
                       "SOI row is selectable!")
         # assert rows for submissions with missing location data are not
         # clickable
