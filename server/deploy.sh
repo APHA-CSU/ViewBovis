fc9ac326ac15339783c7872ec16cda3cc91f6b0d
ellisrichardj <richard.ellis@apha.gov.uk>
2024-05-22 15:50:27 +0100

    back again

--- server/deploy.sh
+++ server/deploy.sh
@@ -15,9 +15,10 @@ fi
 
 docker run --rm -d --network host --name ${NAME} \
     --mount type=bind,source=${DATA_DIR},target=/data \
-    --mount type=bind,source=${DATA_DIR}/layers,target=/ViewBovis/app/static/data \
-    --mount type=bind,source=/var/log/viewbovis-access.log,target=/ViewBovis/access.log \
-    --mount type=bind,source=/var/log/viewbovis-error.log,target=/ViewBovis/error.log \
     aphacsubot/viewbovis:${IMAGETAG} gunicorn -b 127.0.0.1:${PORT} -w ${WORKERS} deploy:app \
     --log-level=info --access-logfile /ViewBovis/access.log \
     --error-logfile /ViewBovis/error.log
+#    --mount type=bind,source=${DATA_DIR}/layers,target=/ViewBovis/app/static/data \
+#    --mount type=bind,source=/var/log/viewbovis-access.log,target=/ViewBovis/access.log \
+#    --mount type=bind,source=/var/log/viewbovis-error.log,target=/ViewBovis/error.log \
+    
