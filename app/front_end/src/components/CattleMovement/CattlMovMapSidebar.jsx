f017e8e8682b461e8d04bfff5c37e8cb63419efb
Sainab Omar <sainabomar07@gmail.com>
2024-06-24 11:04:38 +0100

    fixes formatting and updates local package.json

--- app/front_end/src/components/CattleMovement/CattlMovMapSidebar.jsx
+++ app/front_end/src/components/CattleMovement/CattlMovMapSidebar.jsx
@@ -6,8 +6,14 @@ import Button from "@govuk-react/button";
 import Heading from "@govuk-react/heading";
 import Input from "@govuk-react/input";
 
-const CattlMovMapSidebar = ({ setSearchSample, handleCheckboxes,checkedLayers, 
-  setSearchSecondSample, countyAndHotspotLayers,setCountyAndHotspotLayers}) => {
+const CattlMovMapSidebar = ({
+  setSearchSample,
+  handleCheckboxes,
+  checkedLayers,
+  setSearchSecondSample,
+  countyAndHotspotLayers,
+  setCountyAndHotspotLayers,
+}) => {
   const [searchInput, setSearchInput] = useState("");
   const [secondSearchInput, setSecondSearchInput] = useState("");
 
@@ -29,7 +35,6 @@ const CattlMovMapSidebar = ({ setSearchSample, handleCheckboxes,checkedLayers,
     setSearchSecondSample(secondSearchInput);
   };
 
-
   return (
     <div>
       <form className="sample-search" onSubmit={handleSubmit}>
@@ -37,106 +42,130 @@ const CattlMovMapSidebar = ({ setSearchSample, handleCheckboxes,checkedLayers,
           <Heading id="enter-sample-container" size="MEDIUM">
             Enter Identifier or Submission
           </Heading>
-          <Input value={searchInput} onChange={handleChange} placeholder="e.g. UK705113600438"/>
+          <Input
+            value={searchInput}
+            onChange={handleChange}
+            placeholder="e.g. UK705113600438"
+          />
           <Button className="my-2" buttonColour="#00a33b">
             Show Cattle Movement
           </Button>
         </div>
       </form>
       <Row className="my-4">
-        <form onSubmit={handleSecondSubmit} >
-        <Accordion>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Add Another Movement</Accordion.Header>
-            <Accordion.Body>
-              <Input value={secondSearchInput} onChange={handleSecondChange} placeholder="e.g. UK705113600438"/>
-              <Button className="my-2" buttonColour="#00a33b">
-            Show Second Movement
-          </Button>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <form onSubmit={handleSecondSubmit}>
+          <Accordion>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Add Another Movement</Accordion.Header>
+              <Accordion.Body>
+                <Input
+                  value={secondSearchInput}
+                  onChange={handleSecondChange}
+                  placeholder="e.g. UK705113600438"
+                />
+                <Button className="my-2" buttonColour="#00a33b">
+                  Show Second Movement
+                </Button>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </form>
       </Row>
       <Row className="my-5">
         <Heading size="SMALL">LAYERS</Heading>
         <Accordion>
-  <Accordion.Item eventKey="1">
-    <Accordion.Header>
-      <Form.Check
-        className="checkbox"
-        label="Risk Areas"
-        checked={checkedLayers["showAllRA"]}
-        onChange={() => handleCheckboxes(0)}
-      />
-    </Accordion.Header>
-    <Accordion.Body>
-      <div>
-        <strong>England</strong>
-        <Form.Check
-          className="checkbox"
-          label="HRA"
-          checked={checkedLayers["High Risk Area"]}
-          onChange={() => {handleCheckboxes(1)}}
-        />
-        <Form.Check
-          className="checkbox"
-          label="LRA"
-          onChange={() => handleCheckboxes(2)}
-          checked={checkedLayers["Low Risk Area"]}
-        />
-        <Form.Check
-          className="checkbox"
-          label="Edge"
-          onChange={() => handleCheckboxes(3)}
-          checked={checkedLayers["Edge Area"]}
-        />
-      </div>
-      <div>
-        <strong>Wales</strong>
-        <Form.Check
-          className="checkbox"
-          label="HTBA"
-          onChange={() => handleCheckboxes(4)}
-          checked={checkedLayers["High TB Area"]}
-        />
-        <Form.Check
-          className="checkbox"
-          label="ITBA"
-          onChange={() => handleCheckboxes(5)}
-          checked={checkedLayers["Intermediate TB Area"]}
-        />
-        <Form.Check
-          className="checkbox"
-          label="LTBA"
-          onChange={() => handleCheckboxes(6)}
-          checked={checkedLayers["Low TB Area"]}
-        />
-      </div>
-      <div>
-        <strong>Scotland</strong>
-        <Form.Check
-          className="checkbox"
-          label="TBFA"
-          onChange={() => handleCheckboxes(7)}
-          checked={checkedLayers["TB Free Area"]}
-        />
-      </div>
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-<Form>
-  <Form.Check className="checkbox" label="Counties" 
-    checked={countyAndHotspotLayers["countyLayers"]}
-    onChange={() => {setCountyAndHotspotLayers({...countyAndHotspotLayers,
-      "countyLayers" : !(countyAndHotspotLayers["countyLayers"])})}}/>
-</Form>
-<Form>
-    <Form.Check className="checkbox" label="TB Hotspots"
-      checked={countyAndHotspotLayers["hotspotLayers"]} 
-      onChange={() => {setCountyAndHotspotLayers({...countyAndHotspotLayers,
-        "hotspotLayers" : !(countyAndHotspotLayers["hotspotLayers"])})}}/>
-</Form>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>
+              <Form.Check
+                className="checkbox"
+                label="Risk Areas"
+                checked={checkedLayers["showAllRA"]}
+                onChange={() => handleCheckboxes(0)}
+              />
+            </Accordion.Header>
+            <Accordion.Body>
+              <div>
+                <strong>England</strong>
+                <Form.Check
+                  className="checkbox"
+                  label="HRA"
+                  checked={checkedLayers["High Risk Area"]}
+                  onChange={() => {
+                    handleCheckboxes(1);
+                  }}
+                />
+                <Form.Check
+                  className="checkbox"
+                  label="LRA"
+                  onChange={() => handleCheckboxes(2)}
+                  checked={checkedLayers["Low Risk Area"]}
+                />
+                <Form.Check
+                  className="checkbox"
+                  label="Edge"
+                  onChange={() => handleCheckboxes(3)}
+                  checked={checkedLayers["Edge Area"]}
+                />
+              </div>
+              <div>
+                <strong>Wales</strong>
+                <Form.Check
+                  className="checkbox"
+                  label="HTBA"
+                  onChange={() => handleCheckboxes(4)}
+                  checked={checkedLayers["High TB Area"]}
+                />
+                <Form.Check
+                  className="checkbox"
+                  label="ITBA"
+                  onChange={() => handleCheckboxes(5)}
+                  checked={checkedLayers["Intermediate TB Area"]}
+                />
+                <Form.Check
+                  className="checkbox"
+                  label="LTBA"
+                  onChange={() => handleCheckboxes(6)}
+                  checked={checkedLayers["Low TB Area"]}
+                />
+              </div>
+              <div>
+                <strong>Scotland</strong>
+                <Form.Check
+                  className="checkbox"
+                  label="TBFA"
+                  onChange={() => handleCheckboxes(7)}
+                  checked={checkedLayers["TB Free Area"]}
+                />
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <Form>
+          <Form.Check
+            className="checkbox"
+            label="Counties"
+            checked={countyAndHotspotLayers["countyLayers"]}
+            onChange={() => {
+              setCountyAndHotspotLayers({
+                ...countyAndHotspotLayers,
+                countyLayers: !countyAndHotspotLayers["countyLayers"],
+              });
+            }}
+          />
+        </Form>
+        <Form>
+          <Form.Check
+            className="checkbox"
+            label="TB Hotspots"
+            checked={countyAndHotspotLayers["hotspotLayers"]}
+            onChange={() => {
+              setCountyAndHotspotLayers({
+                ...countyAndHotspotLayers,
+                hotspotLayers: !countyAndHotspotLayers["hotspotLayers"],
+              });
+            }}
+          />
+        </Form>
         <Form>
           <Form.Check className="checkbox" label="Movement Lines" />
         </Form>
--- app/front_end/src/components/CattleMovement/CattleMovement.jsx
+++ app/front_end/src/components/CattleMovement/CattleMovement.jsx
@@ -9,14 +9,16 @@ import "./CattleMovement.css";
 const CattleMovement = () => {
   const [searchSample, setSearchSample] = useState("");
   const [jsonData, setjsonData] = useState({});
-  const [countyAndHotspotLayers,setCountyAndHotspotLayers] = useState({
-    "hotspotLayers":false,"countyLayers":false})
-  const [checkedLayers, setCheckedLayers] = useState({})
+  const [countyAndHotspotLayers, setCountyAndHotspotLayers] = useState({
+    hotspotLayers: false,
+    countyLayers: false,
+  });
+  const [checkedLayers, setCheckedLayers] = useState({});
 
   const handleCheckboxes = (index) => {
-    switch(index) {
+    switch (index) {
       case 0:
-        checkedLayers["showAllRA"] = !checkedLayers["showAllRA"]
+        checkedLayers["showAllRA"] = !checkedLayers["showAllRA"];
         checkedLayers["High Risk Area"] = checkedLayers["showAllRA"];
         checkedLayers["Low Risk Area"] = checkedLayers["showAllRA"];
         checkedLayers["Edge Area"] = checkedLayers["showAllRA"];
@@ -26,10 +28,10 @@ const CattleMovement = () => {
         checkedLayers["TB Free Area"] = checkedLayers["showAllRA"];
         break;
       case 1:
-        checkedLayers["High Risk Area"] =  !checkedLayers["High Risk Area"];
+        checkedLayers["High Risk Area"] = !checkedLayers["High Risk Area"];
         break;
       case 2:
-        checkedLayers["Low Risk Area"] =  !checkedLayers["Low Risk Area"];
+        checkedLayers["Low Risk Area"] = !checkedLayers["Low Risk Area"];
         break;
       case 3:
         checkedLayers["Edge Area"] = !checkedLayers["Edge Area"];
@@ -38,7 +40,8 @@ const CattleMovement = () => {
         checkedLayers["High TB Area"] = !checkedLayers["High TB Area"];
         break;
       case 5:
-        checkedLayers["Intermediate TB Area"] = !checkedLayers["Intermediate TB Area"];
+        checkedLayers["Intermediate TB Area"] =
+          !checkedLayers["Intermediate TB Area"];
         break;
       case 6:
         checkedLayers["Low TB Area"] = !checkedLayers["Low TB Area"];
@@ -46,14 +49,13 @@ const CattleMovement = () => {
       case 7:
         checkedLayers["TB Free Area"] = !checkedLayers["TB Free Area"];
         break;
-  }
-    setCheckedLayers({...checkedLayers})
-  }
-
+    }
+    setCheckedLayers({ ...checkedLayers });
+  };
 
   const [searchSecondSample, setSearchSecondSample] = useState("");
   const [secondJsonData, setSecondjsonData] = useState({});
-  
+
   //Fetch sample data and store in jsonData
   useEffect(() => {
     if (searchSample)
@@ -91,29 +93,29 @@ const CattleMovement = () => {
   }, [searchSecondSample]);
 
   return (
-    <div className='container-fluid content'>
-    <Container fluid id="custom-container" data-testid="cattlemovement-1">
-      <Row>
-        <Col className="sidebar col-3">
-          <MapSidebar
-            setSearchSample={setSearchSample}
-            handleCheckboxes = {handleCheckboxes}
-            checkedLayers={checkedLayers}
-            setSearchSecondSample={setSearchSecondSample}
-            countyAndHotspotLayers={countyAndHotspotLayers} 
-            setCountyAndHotspotLayers={setCountyAndHotspotLayers}
-          />
-        </Col>
-        <Col>
-          <CattleMovementMap
-            jsonData={jsonData}
-            checkedLayers={checkedLayers}
-            secondJsonData={secondJsonData}
-            useCountyandHotspotLayers={countyAndHotspotLayers}
-          />
-        </Col>
-      </Row>
-    </Container>
+    <div className="container-fluid content">
+      <Container fluid id="custom-container" data-testid="cattlemovement-1">
+        <Row>
+          <Col className="sidebar col-3">
+            <MapSidebar
+              setSearchSample={setSearchSample}
+              handleCheckboxes={handleCheckboxes}
+              checkedLayers={checkedLayers}
+              setSearchSecondSample={setSearchSecondSample}
+              countyAndHotspotLayers={countyAndHotspotLayers}
+              setCountyAndHotspotLayers={setCountyAndHotspotLayers}
+            />
+          </Col>
+          <Col>
+            <CattleMovementMap
+              jsonData={jsonData}
+              checkedLayers={checkedLayers}
+              secondJsonData={secondJsonData}
+              useCountyandHotspotLayers={countyAndHotspotLayers}
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
