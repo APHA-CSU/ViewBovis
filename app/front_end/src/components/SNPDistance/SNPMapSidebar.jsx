2786e4201a481ee9d7d272954fa7ffe4256bc871
praveenmurugan101 <praveenmurugan@ranch-653.int.sce.network>
2024-07-01 10:57:13 +0100

    Layers Checkbox UI and SNP matrix

--- app/front_end/src/components/SNPDistance/SNPMapSidebar.jsx
+++ app/front_end/src/components/SNPDistance/SNPMapSidebar.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import Row from "react-bootstrap/Row";
-import Accordion from "react-bootstrap/Accordion";
-import Form from "react-bootstrap/Form";
-import Button from "@govuk-react/button";
-import Heading from "@govuk-react/heading";
-import Input from "@govuk-react/input";
+import LayersCheckbox from "../Layers/LayersCheckbox";
 
-const SNPMapSidebar = ({ fetchSNPMapDataset,handleCheckboxes,checkedLayers, 
-  countyAndHotspotLayers,setCountyAndHotspotLayers }) => {
+const SNPMapSidebar = ({
+  fetchSNPMapDataset,
+  handleCheckboxes,
+  checkedLayers,
+  countyAndHotspotLayers,
+  setCountyAndHotspotLayers,
+}) => {
   const [snpSearchInput, setSnpSearchInput] = useState("");
   const [snpDistance, setSnpDistance] = useState(1);
 
@@ -21,117 +21,68 @@ const SNPMapSidebar = ({ fetchSNPMapDataset,handleCheckboxes,checkedLayers,
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetchSNPMapDataset(snpSearchInput,snpDistance);
+    fetchSNPMapDataset(snpSearchInput, snpDistance);
   };
 
   return (
     <div>
       <form className="sample-search" onSubmit={handleSubmit}>
         <div>
-          <Heading id="enter-sample-container" size="MEDIUM">
+          <div className="govuk-heading-m" id="enter-sample-container">
             Enter Identifier or Submission
-          </Heading>
-          <Input value={snpSearchInput} onChange={handleChange} />
-          <Heading id="enter-sample-container" size="MEDIUM" style={{ marginTop: "10px"}}>
-          Select SNP Distance
-          </Heading>
-          <div style={{marginTop:"20px"}}>
-          <p className="display-value-fixed" style={{float: "left"}}>0</p>
-          <p className="display-value-fixed" style={{float: "right"}}>10</p>
           </div>
-          <input className="snp-slider" type="range" onChange={handleSlider} step="1" 
-          defaultValue="1" max="10" min="0"/>
-          <br/>
-          SNP Distance: {snpDistance}
-          <br/>
-          <Button className="my-2" buttonColour="#00a33b">
-          Plot Related Isolates
-          </Button>
+          <input
+            type="text"
+            className="input__sampleID"
+            id="input__sampleID_temp--1"
+            placeholder="e.g. UK705113600438"
+            name="Name"
+            title="Identifier or Submission Number"
+            value={snpSearchInput}
+            onChange={handleChange}
+          />
+          <br></br>
+          <br></br>
+          <div id="enter-sample-container" className="govuk-heading-m">
+            Select SNP Distance
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <p className="display-value-fixed" style={{ float: "left" }}>
+              0
+            </p>
+            <p className="display-value-fixed" style={{ float: "right" }}>
+              10
+            </p>
+          </div>
+          <input
+            className="snp-slider"
+            type="range"
+            onChange={handleSlider}
+            step="1"
+            defaultValue="1"
+            max="10"
+            min="0"
+          />
+          <br></br>
+          <span style={{ fontSize: "20px" }}>
+            {" "}
+            <b>SNP Distance:</b> {snpDistance}{" "}
+          </span>
+          <br></br>
+          <br></br>
+          <button
+            className="govuk-button"
+            id="btn__plot-related-isolates"
+            aria-disabled="true"
+          >
+            Plot Related Isolates
+          </button>
         </div>
       </form>
-      <Row className="my-5">
-        <Heading size="SMALL">LAYERS</Heading>
-        <Form>
-        <Accordion>
-  <Accordion.Item eventKey="1">
-    <Accordion.Header>
-      <Form.Check
-        className="checkbox"
-        label="Risk Areas"
-        checked={checkedLayers["showAllRA"]}
-        onChange={() => handleCheckboxes(0)}
-      />
-    </Accordion.Header>
-    <Accordion.Body>
-      <div>
-        <strong>England</strong>
-        <Form.Check
-          className="checkbox"
-          label="HRA"
-          checked={checkedLayers["High Risk Area"]}
-          onChange={() => {handleCheckboxes(1)}}
-        />
-        <Form.Check
-          className="checkbox"
-          label="LRA"
-          onChange={() => handleCheckboxes(2)}
-          checked={checkedLayers["Low Risk Area"]}
-        />
-        <Form.Check
-          className="checkbox"
-          label="Edge"
-          onChange={() => handleCheckboxes(3)}
-          checked={checkedLayers["Edge Area"]}
-        />
-      </div>
-      <div>
-        <strong>Wales</strong>
-        <Form.Check
-          className="checkbox"
-          label="HTBA"
-          onChange={() => handleCheckboxes(4)}
-          checked={checkedLayers["High TB Area"]}
-        />
-        <Form.Check
-          className="checkbox"
-          label="ITBA"
-          onChange={() => handleCheckboxes(5)}
-          checked={checkedLayers["Intermediate TB Area"]}
-        />
-        <Form.Check
-          className="checkbox"
-          label="LTBA"
-          onChange={() => handleCheckboxes(6)}
-          checked={checkedLayers["Low TB Area"]}
-        />
-      </div>
-      <div>
-        <strong>Scotland</strong>
-        <Form.Check
-          className="checkbox"
-          label="TBFA"
-          onChange={() => handleCheckboxes(7)}
-          checked={checkedLayers["TB Free Area"]}
-        />
-      </div>
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-<Form>
-  <Form.Check className="checkbox" label="Counties" 
-    checked={countyAndHotspotLayers["countyLayers"]}
-    onChange={() => {setCountyAndHotspotLayers({...countyAndHotspotLayers,
-      "countyLayers" : !(countyAndHotspotLayers["countyLayers"])})}}/>
-</Form>
-<Form>
-    <Form.Check className="checkbox" label="TB Hotspots"
-      checked={countyAndHotspotLayers["hotspotLayers"]} 
-      onChange={() => {setCountyAndHotspotLayers({...countyAndHotspotLayers,
-        "hotspotLayers" : !(countyAndHotspotLayers["hotspotLayers"])})}}/>
-</Form>
-        </Form>
-      </Row>
-      <hr></hr>
+      <LayersCheckbox checkedLayers={checkedLayers} 
+      countyAndHotspotLayers={countyAndHotspotLayers}
+      handleCheckboxes={handleCheckboxes}
+      setCountyAndHotspotLayers={setCountyAndHotspotLayers} />
     </div>
   );
 };
--- app/front_end/src/components/SNPDistance/SNPMatrix.jsx
+++ app/front_end/src/components/SNPDistance/SNPMatrix.jsx
@@ -1,20 +1,56 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import SNPMatrixSideBar from './SNPMatrixSideBar.jsx';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import SNPMatrixSideBar from "./SNPMatrixSideBar.jsx";
+import SNPMatrixComp from "./SNPMatrixComp.jsx";
+import { useState } from "react";
 
 const SNPMatrix = () => {
-  return  (<div className='container-fluid content'>
-  <Container fluid id="custom-container">
-  <Row>
-    <Col className="sidebar col-3">
-      <SNPMatrixSideBar />
-    </Col>
-    <Col>
+  const [matrixSNPDistance, setMatrixSNPDistance] = useState(1);
+  const [matrixSampleSelected, SetMatrixSampleSelected] = useState("");
+  const [matrixJson, setMatrixJson] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const handlePlotMatrix = async (sample, distance) => {
+    // Fetch json data from backend
+    const response = await fetch(
+      `/sample/matrix?sample_name=${matrixSampleSelected}&snp_distance=${matrixSNPDistance}`
+    );
 
-    </Col>
-  </Row>
-</Container>
-</div>)
+    if (!response.ok) {
+      setErrorMessage("Server Error");
+    } else {
+      const json = await response.json();
+
+      // Remove spinner when fetch is complete
+      //document.getElementById("snpmatrix-spinner").classList.add("hidden");
+
+      // If response contains a warning
+      if (json["warnings"]) {
+        setErrorMessage(`
+          <p class="warning-text" style="white-space:pre" id="snpmatrix-error-message">${json["warning"]}</p>
+        `);
+      } else {
+        setMatrixJson(json);
+      }
+    }
+  };
+  return (
+    <div className="container-fluid content">
+      <Container fluid id="custom-container">
+        <Row>
+          <Col className="sidebar col-3">
+            <SNPMatrixSideBar handlePlotMatrix={handlePlotMatrix} />
+          </Col>
+          <Col>
+            <SNPMatrixComp
+              matrixSampleSelected={matrixSampleSelected}
+              matrixSNPDistance={matrixSNPDistance}
+              json={matrixJson}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 export default SNPMatrix;
