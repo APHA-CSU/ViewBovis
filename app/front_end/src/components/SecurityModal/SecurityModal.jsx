0fed500e80fd0c5e9b09c94fd3db8429e496042c
praveenmurugan101 <praveenmurugan@ranch-653.int.sce.network>
2024-09-17 16:39:43 +0100

    e2e test fixes for snpmap tab

--- app/front_end/src/components/SecurityModal/SecurityModal.jsx
+++ app/front_end/src/components/SecurityModal/SecurityModal.jsx
@@ -12,81 +12,87 @@ const SecurityModal = () => {
     document.body.style.overflow = "hidden";
   }, []);
 
-
   return (
-    <div
-      role="alertdialog"
-      aria-label="Disclaimer"
-      className="modal fade show"
-      style={{
-        display: showModal ? "block" : "none",
-        backgroundColor: "rgba(0,0,0,0.9)",
-      }}
-    >
-      <div className="modal-dialog modal-dialog-centered modal-lg">
+    <>
+      {showModal && (
         <div
-          className="modal-content"
+          role="alertdialog"
+          aria-label="Disclaimer"
+          className="modal fade show"
           style={{
-            backgroundColor: "transparent",
-            border: "none",
-            width: "fit-content",
+            display: showModal ? "block" : "none",
+            backgroundColor: "rgba(0,0,0,0.9)",
           }}
         >
-          <div className="modal-body">
-            <div className="govuk-notification-banner govuk-notification-banner--success">
-              <div className="govuk-notification-banner__header">
-                <h2
-                  className="govuk-notification-banner__title"
-                  id="govuk-notification-banner-title"
-                >
-                  Important Information
-                </h2>
-              </div>
-              <div className="govuk-notification-banner__content">
-                <div className="govuk-warning-text">
-                  <span className="govuk-warning-text__icon" aria-hidden="true">
-                    !
-                  </span>
-                  <strong className="govuk-warning-text__text">
-                    Do not share login details between users
-                  </strong>
-                </div>
-                <div>
-                  <p>
-                    ViewBovis displays Official Sensitive data which must only
-                    be viewed by those with appropriate access. Failure to
-                    comply risks shutdown of the ViewBovis service by APHA IT,
-                    due to mishandling of Official Sensitive data.
-                  </p>
-                </div>
-                <div className="govuk-form-group">
-                  <div
-                    className="govuk-checkboxes govuk-checkboxes--small"
-                    data-module="govuk-checkboxes"
-                  >
-                    <div
-                      className="govuk-checkboxes__item"
-                      style={{ marginLeft: "33%" }}
+          <div className="modal-dialog modal-dialog-centered modal-lg">
+            <div
+              className="modal-content"
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                width: "fit-content",
+              }}
+            >
+              <div className="modal-body">
+                <div className="govuk-notification-banner govuk-notification-banner--success">
+                  <div className="govuk-notification-banner__header">
+                    <h2
+                      className="govuk-notification-banner__title"
+                      id="govuk-notification-banner-title"
                     >
-                      <input
-                        className="govuk-checkboxes__input"
-                        type="checkbox"
-                        checked={checked}
-                        onChange={() => {
-                          setChecked(true);
-                          setTimeout(()=>{
-                            dispatch(hideModal())
-                            document.body.style.overflow = "auto";
-                          },2000)
-                        }}
-                        disabled={checked}
-                      />
-                      <label
-                        className="govuk-label govuk-checkboxes__label"
-                        htmlFor="checkbox--agree"
+                      Important Information
+                    </h2>
+                  </div>
+                  <div className="govuk-notification-banner__content">
+                    <div className="govuk-warning-text">
+                      <span
+                        className="govuk-warning-text__icon"
+                        aria-hidden="true"
                       >
-                        I agree to the above
-                      </label>
+                        !
+                      </span>
+                      <strong className="govuk-warning-text__text">
+                        Do not share login details between users
+                      </strong>
+                    </div>
+                    <div>
+                      <p>
+                        ViewBovis displays Official Sensitive data which must
+                        only be viewed by those with appropriate access. Failure
+                        to comply risks shutdown of the ViewBovis service by
+                        APHA IT, due to mishandling of Official Sensitive data.
+                      </p>
+                    </div>
+                    <div className="govuk-form-group">
+                      <div
+                        className="govuk-checkboxes govuk-checkboxes--small"
+                        data-module="govuk-checkboxes"
+                      >
+                        <div
+                          className="govuk-checkboxes__item"
+                          style={{ marginLeft: "33%" }}
+                        >
+                          <input
+                            className="govuk-checkboxes__input"
+                            type="checkbox"
+                            checked={checked}
+                            onChange={() => {
+                              setChecked(true);
+                              setTimeout(() => {
+                                dispatch(hideModal());
+                                document.body.style.overflow = "auto";
+                              }, 2000);
+                            }}
+                            disabled={checked}
+                          />
+                          <label
+                            className="govuk-label govuk-checkboxes__label"
+                            htmlFor="checkbox--agree"
+                          >
+                            I agree to the above
+                          </label>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -94,8 +100,8 @@ const SecurityModal = () => {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- app/tests/e2e_tests.py
+++ app/tests/e2e_tests.py
@@ -46,10 +46,8 @@ class E2ETests(unittest.TestCase):
         related_nonplots = ["e", "f"]
         distant_relations = ["g", "h"]
         # navigate to snp map
-        snp_dist_btn = self.driver.find_element(By.ID, "snp_distance_tab")
+        snp_dist_btn = self.wait.until(EC.element_to_be_clickable((By.ID,"snp_distance_tab")))
         snp_dist_btn.click()
-        snp_map_btn = self.wait.until(EC.element_to_be_clickable((By.ID,"btn-view-snpmap")))
-        snp_map_btn.click()
         # search for related samples within 5 SNP of "a_id"
         search_box = self.driver.find_element(By.ID, "input__sampleID_temp--1")
         search_box.send_keys(f"{soi}_id")
