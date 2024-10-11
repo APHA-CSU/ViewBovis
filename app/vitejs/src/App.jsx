628f6cd6545b06d9dcb8657c5cbb1f048dcda0c3
Praveen Murugan <praveenmurugan@ranch-743.int.sce.network>
2024-10-11 12:48:42 +0100

    migrates from react scripts to vitejs

--- app/vitejs/src/App.jsx
+++ app/vitejs/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./components/Home/Home.jsx";
+import CattleMovement from "./components/CattleMovement/CattleMovement.jsx";
+import SNPMap from "./components/SNPDistance/SNPMap.jsx";
+import Nextstrain from "./components/Nextstrain/Nextstrain.jsx";
+import HelpSupport from "./components/HelpSupport/HelpSupport.jsx";
+import NavbarComp from "./components/Navbar/NavbarComp.jsx";
+import SecurityModal from "./components/SecurityModal/SecurityModal.jsx";
+import { lazy, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setShowLayers } from "./features/counter/securitySlice.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* Lazy loading the layers*/
+  const dispatch = useDispatch();
+  const SNPMapComp = lazy(() =>
+    import("./components/SNPDistance/SNPMapComp.jsx")
+  );
+  const CattleMovementMap = lazy(() =>
+    import("./components/CattleMovement/CattleMovementMap.jsx")
+  );
+  const RiskLayers = lazy(() => import("./components/Layers/RiskLayers.jsx"));
+  const CountyLayers = lazy(() => import("./components/Layers/CountyLayers.jsx"));
+  const HotspotLayers = lazy(() => import("./components/Layers/HotspotLayers.jsx"));
+  const showHelpSupportPage = useSelector(
+    (state) => state.security.showHelpSupportPage
+  );
+
+  useEffect(() => {
+    /* Preload the layers on mount*/
+    (async () => {
+      await import("./components/Layers/RiskLayers.jsx");
+      await import("./components/Layers/CountyLayers.jsx");
+      await import("./components/Layers/HotspotLayers.jsx");
+      dispatch(setShowLayers(true));
+    })();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SecurityModal />
+      <NavbarComp />
+      <Home />
+      <SNPMap
+        SNPMapComp={SNPMapComp}
+        RiskLayers={RiskLayers}
+        CountyLayers={CountyLayers}
+        HotspotLayers={HotspotLayers}
+      />
+      <CattleMovement
+        CattleMovementMap={CattleMovementMap}
+        RiskLayers={RiskLayers}
+        CountyLayers={CountyLayers}
+        HotspotLayers={HotspotLayers}
+      />
+      <Nextstrain />
+      {showHelpSupportPage && <HelpSupport />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
