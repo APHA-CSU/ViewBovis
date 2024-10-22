9b82a1afb445c3b34f6eb2fe00cfd8a96b912eaf
Praveen Murugan <praveenmurugan@ranch-743.int.sce.network>
2024-10-22 16:02:54 +0100

    initialises CPH table component

---
+++ app/front_end/src/components/CPHSearch/CPHTableComp.jsx
@@ -0,0 +1,111 @@
+import { ReactTabulator } from "react-tabulator";
+
+const CPHTableComp = ({ samples }) => {
+  const tabledata = [...samples].map((sampleObj) => {
+    return sampleObj;
+  });
+  const columns = [
+    {
+      title: "Submission",
+      field: "Submission",
+      headerFilter: "input",
+      sorter: "string",
+      width: 120,
+    },
+    {
+      title: "Identifier",
+      field: "Identifier",
+      headerFilter: "input",
+      sorter: "string",
+      width: 130,
+    },
+    {
+      title: "Clade",
+      field: "Clade",
+      headerFilter: "input",
+      sorter: "string",
+      width: 100,
+    },
+    {
+      title: "Birth Location",
+      field: "Loc0",
+      headerFilter: "input",
+      sorter: "string",
+      width: 120,
+    },
+    {
+      title: "Date of Birth",
+      field: "wsdBirthDate",
+      headerFilter: "input",
+      sorter: "string",
+      width: 120,
+    },
+    {
+      title: "Slaughter Date",
+      field: "SlaughterDate",
+      headerFilter: "input",
+      sorter: "string",
+      width: 130,
+    },
+    {
+      title: "Sex",
+      field: "Gender",
+      headerFilter: "input",
+      sorter: "string",
+      width: 70,
+    },
+    {
+      title: "Disclosing Test Type",
+      field: "Disclosing_Test",
+      headerFilter: "input",
+      sorter: "string",
+      width: 150,
+    },
+    {
+      title: "Import Country",
+      field: "Import_Country",
+      headerFilter: "input",
+      sorter: "string",
+      width: 120,
+    },
+    {
+      title: "Launch Tools",
+      field: "tools",
+      width: 200,
+      formatter: (cell) => {
+        let list = document.createElement("div");
+        list.innerHTML =
+          "<ul><li>SNP Map</li><li>Cattle Movement Map</li><li>Nextstrain</li></ul>";
+        return list;
+      },
+    },
+  ];
+  return (
+    <>
+      {samples.length > 0 && (
+        <>
+          <b>CPH : {samples[0]["CPH"]}</b>
+          <br></br>
+          <div
+            className="cphtable-container"
+            style={{
+              overflowX: "scroll",
+              overflowY: "scroll",
+              border: "1px solid black",
+            }}
+          >
+            <div style={{ maxHeight: "50vh", width:"1280px" }}>
+              <ReactTabulator
+                style={{ fontSize: "11px" }}
+                columns={columns}
+                data={tabledata}
+              />
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default CPHTableComp;
