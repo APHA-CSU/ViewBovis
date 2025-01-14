87267c7bee9d6242607307c6a393354b29d08d83
Praveen Murugan <praveenmurugan@ranch-743.int.sce.network>
2025-01-14 13:01:36 +0000

    creates table according to user viewport

--- app/front_end/src/components/CPHSearch/CPHTableComp.jsx
+++ app/front_end/src/components/CPHSearch/CPHTableComp.jsx
@@ -12,68 +12,68 @@ const CPHTableComp = () => {
       field: "Submission",
       headerFilter: "input",
       sorter: "string",
-      width: 120,
+      width: 120 > 0.12 * window.innerWidth ? 120 :  0.12 * window.innerWidth
     },
     {
       title: "Identifier",
       field: "Identifier",
       headerFilter: "input",
       sorter: "string",
-      width: 130,
+      width: 130 > 0.13 * window.innerWidth ? 130 :  0.13 * window.innerWidth ,
     },
     {
       title: "Clade",
       field: "Clade",
       headerFilter: "input",
       sorter: "string",
-      width: 100,
+      width: 100 > 0.1 * window.innerWidth ? 100 :  0.1 * window.innerWidth,
     },
     {
       title: "Birth Location",
       field: "Loc0",
       headerFilter: "input",
       sorter: "string",
-      width: 120,
+      width: 120 > 0.12 * window.innerWidth ? 120 :  0.12 * window.innerWidth,
     },
     {
       title: "Date of Birth",
       field: "wsdBirthDate",
       headerFilter: "input",
       sorter: "string",
-      width: 120,
+      width: 120 > 0.12 * window.innerWidth ? 120 :  0.12 * window.innerWidth,
     },
     {
       title: "Slaughter Date",
       field: "SlaughterDate",
       headerFilter: "input",
       sorter: "string",
-      width: 130,
+      width: 130 > 0.13 * window.innerWidth ? 130 :  0.13 * window.innerWidth,
     },
     {
       title: "Sex",
       field: "Gender",
       headerFilter: "input",
       sorter: "string",
-      width: 70,
+      width: 70 > 0.07 * window.innerWidth ? 70 :  0.07 * window.innerWidth,
     },
     {
       title: "Disclosing Test Type",
       field: "Disclosing_Test",
       headerFilter: "input",
       sorter: "string",
-      width: 150,
+      width: 150 > 0.15 * window.innerWidth ? 150 :  0.15 * window.innerWidth,
     },
     {
       title: "Import Country",
       field: "Import_Country",
       headerFilter: "input",
       sorter: "string",
-      width: 120,
+      width: 120 > 0.12 * window.innerWidth ? 120 :  0.12 * window.innerWidth,
     },
     {
       title: "Launch Tools",
       field: "tools",
-      width: 200,
+      width: 200 > 0.2 * window.innerWidth ? 200 :  0.2 * window.innerWidth,
       formatter: (cell) => {
         let data = cell.getValue();
         if (data["warnings"]) {
@@ -121,9 +121,9 @@ const CPHTableComp = () => {
               border: "1px solid black",
             }}
           >
-            <div style={{ maxHeight: "50vh", width: "1280px" }}>
+            <div style={{ maxHeight: "50vh", width: 1280 > 1.26 * window.innerWidth ? 1280 : 1.26 * window.innerWidth}}>
               <ReactTabulator
-                style={{ fontSize: "11px" }}
+                style={{ fontSize: 11 > 0.01*window.innerWidth ? 11 : 0.01*window.innerWidth }}
                 columns={columns}
                 data={tabledata}
               />
