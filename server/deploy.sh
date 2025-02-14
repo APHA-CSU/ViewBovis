03e310a013cbcfdca6136024c654afae381ffd7d
ellisrichardj <richard.ellis@apha.gov.uk>
2025-02-14 22:46:37 +0000

    Add training option for startup

--- server/deploy.sh
+++ server/deploy.sh
@@ -2,17 +2,23 @@
 
 DATA_DIR=$1
 if [ $2 = 'testing' ]
-then
-NAME='viewbovis-test'
-IMAGETAG='prod-test'
-PORT='3002'
-WORKERS='1'
+    then
+    NAME='viewbovis-test'
+    IMAGETAG='prod-test'
+    PORT='3002'
+    WORKERS='1'
 elif [ $2 = 'prod' ]
-then
-NAME='viewbovis'
-IMAGETAG='prod'
-PORT='3000'
-WORKERS='8'
+    then
+    NAME='viewbovis'
+    IMAGETAG='prod'
+    PORT='3000'
+    WORKERS='6'
+elif [ $2 = 'training']
+    then
+    NAME='viewbovis-training'
+    IMAGETAG='prod'
+    PORT='3001'
+    WORKERS='2'
 fi
 
 docker run --rm -d --network host --name ${NAME} \
@@ -21,5 +27,5 @@ docker run --rm -d --network host --name ${NAME} \
     --mount type=bind,source=/var/log/viewbovis-access.log,target=/ViewBovis/access.log \
     --mount type=bind,source=/var/log/viewbovis-error.log,target=/ViewBovis/error.log \
     aphacsubot/viewbovis:${IMAGETAG} gunicorn -b 127.0.0.1:${PORT} -w ${WORKERS} deploy:app \
-    --log-level=info --access-logfile /ViewBovis/access.log \
-    --error-logfile /ViewBovis/error.log
+    --log-level=info --access-logfile /ViewBovis/$2-access.log \
+    --error-logfile /ViewBovis/$2-error.log
